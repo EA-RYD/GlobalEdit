9a0620a1474ad996ba0a61b09ea49340633edb2d
ea-ryd <eric.arreola@sjsu.edu>
2022-11-02 22:04:31 -0700

    Testing yjs binding

--- globaledit/src/App.js
+++ globaledit/src/App.js
@@ -2,15 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import Editor, { DiffEditor, useMonaco, loader } from "@monaco-editor/react";
 import { useEffect, useRef, useState } from 'react';
+import { WebrtcProvider } from 'y-webrtc'
+import * as Y from 'yjs'
+import { IndexeddbPersistence } from 'y-indexeddb'
+import { MonacoBinding } from 'y-monaco' 
+import RandomColor from "randomcolor";
 //import loader from "@monaco-editor/loader";
 
-function App() {
+function App() {  
   const [contentMarkdown, setContentMarkdown] = useState('//type here');
-  const editorRef = useRef(null);
+  const [editorRef, setEditorRef] = useState("");
   const monacoRef = useRef(null);
 
   function handleEditorDidMount(editor, monaco) {
-    editorRef.current = editor;
+    setEditorRef(editor);
     monacoRef.current = monaco;
   }
 
@@ -30,7 +35,41 @@ function App() {
   }
 
   
+  useEffect(() => {
+    if (editorRef) {
+      const ydoc = new Y.Doc();
+      let webrtcProvider = null;
 
+      // Sync clients with the y-webrtc provider.
+      try {
+        webrtcProvider = new WebrtcProvider('globaledit', ydoc,{signaling: [
+          "wss://signaling.yjs.dev",
+          'wss://y-webrtc-signaling-eu.herokuapp.com', 
+          'wss://y-webrtc-signaling-us.herokuapp.com'
+        ]});
+        const type = ydoc.getText('monaco');
+        const yUndoManager = new Y.UndoManager(type);
+        const aware = webrtcProvider.awareness;
+        const color = RandomColor();
+
+        aware.setLocalStateField("user", {
+          name: "Users Name",
+          color: color,
+        });
+        
+        const monacoBinding = new MonacoBinding(type, (editorRef.getModel()), new Set([editorRef]), aware);
+
+      } catch (error) {
+        alert(error)
+      }
+      return () => {
+        if (webrtcProvider) {
+          webrtcProvider.disconnect();
+          ydoc.destroy();
+        }
+      };
+    }
+  }, [editorRef])
 
   return (
     <div className="App">
